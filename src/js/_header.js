8d577057c448703b6297c54d72ceb0b24baaca20
Dmitry Kuzmin <teamspace2028@gmail.com>
2025-09-21 17:12:54 +0300

    init

--- src/js/_header.js
+++ src/js/_header.js
@@ -26,3 +26,38 @@
 // });
 
 // });
+document.addEventListener('DOMContentLoaded', function () {
+  const menuItems = document.querySelectorAll('.menu > li');
+
+  menuItems.forEach(item => {
+    const submenu = item.querySelector('.sub-menu');
+    let timeout;
+
+    if (!submenu) return;
+
+    
+    item.addEventListener('mouseenter', () => {
+      clearTimeout(timeout);
+      submenu.style.display = 'flex';
+    });
+
+   
+    item.addEventListener('mouseleave', () => {
+      timeout = setTimeout(() => {
+        submenu.style.display = 'none';
+      }, 300); 
+    });
+
+   
+    submenu.addEventListener('mouseenter', () => {
+      clearTimeout(timeout);
+    });
+
+    
+    submenu.addEventListener('mouseleave', () => {
+      timeout = setTimeout(() => {
+        submenu.style.display = 'none';
+      }, 300);
+    });
+  });
+});
