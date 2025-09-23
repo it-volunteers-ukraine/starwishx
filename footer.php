2cd2581d6cff44f171e3d1966692e1a015513c93
NilKad <kadulin@gmail.com>
2025-09-23 16:30:54 +0200

    all adaptive

--- footer.php
+++ footer.php
@@ -16,8 +16,8 @@ $linkedin_link = esc_html(get_field('linkedin_link', 'options'));
 
 <footer class="footer">
     <div class="container">
+        <h2 class="footer-title"><?php echo esc_html(get_field('title', 'options')) ?></h2>
         <div class="footer-inner">
-            <h2 class="footer-title"><?php echo esc_html(get_field('title', 'options')) ?></h2>
 
             <ul class="socblock">
                 <?php if ($email_text && $email_link): ?>
@@ -54,38 +54,45 @@ $linkedin_link = esc_html(get_field('linkedin_link', 'options'));
                         </span>
                     </li>
                 <?php endif; ?>
+            </ul>
+
+            <nav class="nav">
+                <?php wp_nav_menu([
+                    'theme_location'       => 'menu-footer',
+                    'container'            => false,
+                    'menu_class'           => 'menu',
+                    'menu_id'              => false,
+                    'echo'                 => true,
+                    'items_wrap'           => '<ul id="%1$s" class="footer_list %2$s">%3$s</ul>',
+                ]);
+                ?>
+            </nav>
         </div>
 
-        <nav class="nav">
-            <?php wp_nav_menu([
-                'theme_location'       => 'menu-footer',
-                'container'            => false,
-                'menu_class'           => 'menu',
-                'menu_id'              => false,
-                'echo'                 => true,
-                'items_wrap'           => '<ul id="%1$s" class="footer_list %2$s">%3$s</ul>',
-            ]);
-            ?>
-        </nav>
+
         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/star_wish_x-1920.png" alt="" class="footer-logo">
         <div class="footer-copyright">
-            <div class="copyright-text">
-                <p class="copyright-text"><?php echo esc_html(get_field('parts_1', 'options')) ?></p>
+            <div class="footer-copyright1">
+                <p class="copyright-text"><?php echo esc_html(get_field('parts_1', 'options')); ?> <span> </span></p>
+                <div class="copyright-text1">
+                    <p class="copyright-text">
+                        <?php echo esc_html(get_field('parts_2', 'options')); ?>
+                        <a href="<?php echo esc_html(get_field('parts_2_link', 'options')); ?>" class="copyright-link"><?php echo esc_html(get_field('parts_2_text_link', 'options')); ?></a>
+                    </p>
+                </div>
             </div>
-            <div class="copyright-text">
-                <?php echo esc_html(get_field('parts_2', 'options')) ?>
-                <a href="<?php echo esc_html(get_field('parts_2_link', 'options')) ?>" class="copyright-link"><?php echo esc_html(get_field('parts_2_text_link', 'options')) ?></a>
+                <div class="footer-copyright2">
+                    <a href="<?php echo esc_html(get_field('privacy_policy_page', 'options')); ?>" class="copyright-link"><?php echo esc_html(get_field('privacy_policy_text', 'options')); ?></a>
+                    <!-- <div class="copyright-text"> -->
+                    <!-- </div> -->
+                    <a href="<?php echo esc_html(get_field('privacy_data_protection_page', 'options')); ?>" class="copyright-link"><?php echo esc_html(get_field('privacy_data_protection_text', 'options')); ?></a>
+                    <!-- <div class="copyright-text"> -->
+                    <!-- </div> -->
+                </div>
+                <?php echo esc_html(get_field('copyright', 'options')); ?>
             </div>
-            <div class="copyright-text">
-                <a href="<?php echo esc_html(get_field('privacy_policy_page', 'options')) ?>" class="copyright-link"><?php echo esc_html(get_field('privacy_policy_text', 'options')) ?></a>
-            </div>
-            <div class="copyright-text">
-                <a href="<?php echo esc_html(get_field('privacy_data_protection_page', 'options')) ?>" class="copyright-link"><?php echo esc_html(get_field('privacy_data_protection_text', 'options')) ?></a>
-            </div>
-            <?php echo esc_html(get_field('copyright', 'options')) ?>
-        </div>
 
-    </div>
+        </div>
 </footer>
 
 <?php wp_footer(); ?>
