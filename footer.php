8e4ff84ce461b7af8356ec6ac79a3f9d4faa7a3a
NilKad <kadulin@gmail.com>
2025-10-22 00:49:06 +0200

    new version 1

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
 <?php
 $footer_title = esc_html(get_field('title', 'options'));
+$socials = get_field('socseti', 'options');
+// print_r($socials);
+$icons_sprites = get_field('icons_sprites', 'options');
+print_r($icons_sprites);
+
 $email_text = esc_html(get_field('email_name', 'options'));
 $email_link = esc_url(get_field('email_link', 'options'));
 $telegram_text = esc_html(get_field('telegram_name', 'options'));
@@ -22,43 +27,71 @@ $linkedin_link = esc_html(get_field('linkedin_link', 'options'));
             <div class="footer-socwraper">
                 <h2 class="footer-title title-socblock"><?php echo $footer_title; ?></h2>
                 <ul class="socblock">
-                    <?php if ($email_text && $email_link): ?>
+                    <?php foreach ($socials as $social) : ?>
+                        <?php
+                        $social_title = esc_html($social['title']);
+                        $social_name_footer = esc_html($social['name_footer']);
+                        $social_link = esc_url($social['value']);
+                        $social_icon = esc_html($social['icon_name_in_sprite']);
+                        
+                        ?>
                         <li class="socblock-item">
-                            <a href="mailto:<?php echo $email_link; ?>" class="socblock-link socblock-link-email" target="_blank" rel="noopener noreferrer">
-                                <svg class="socblock-icon">
-                                    <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprites.svg#icon-email"></use>
-                                    <!-- <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprites.svg#icon-btn"></use> -->
-                                    <!-- <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprites.svg#icon-btn-close"></use> -->
-                                    <!-- <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprites.svg#icon-search"></use> -->
-                                </svg>
-                                <span>
-                                    <?php echo $email_text; ?></a>
-                            </span>
-                        </li>
-                    <?php endif; ?>
-                    <?php if ($telegram_text && $telegram_link): ?>
-                        <li class="socblock-item">
-                            <a href="<?php echo $telegram_link; ?>" class="socblock-link socblock-link-telegram" target="_blank" rel="noopener noreferrer">
-                                <svg class="socblock-icon">
-                                    <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprites.svg#icon-telegram"></use>
-                                </svg>
-                                <span>
-                                    <?php echo $telegram_text; ?></a>
-                            </span>
-                        </li>
+                            <?php if ($social_title == 'Email:') : ?>
+                                <a href="mailto:<?php echo $social_link; ?>" class="socblock-link" target="_blank" rel="noopener noreferrer">
+                                    <svg class="socblock-icon">
+                                        <use xlink:href="<?php echo $icons_sprites; ?>#<?php echo esc_html($social_icon); ?>"></use>
+                                    </svg>
+                                    <span>
+                                        <?php echo $social_name_footer; ?>
+                                    </span>
+                                </a>
+                            <?php else : ?>
+                                <a href="<?php echo $social_link; ?>" class="socblock-link" target="_blank" rel="noopener noreferrer">
+                                    <svg class="socblock-icon">
+                                        <use xlink:href="<?php echo $icons_sprites; ?>#<?php echo esc_html($social_icon); ?>"></use>
+                                    </svg>
+                                    <span>
+                                        <?php echo $social_name_footer; ?>
+                                    </span>
+                                </a>
+                            <?php endif; ?>
 
-                    <?php endif; ?>
-                    <?php if ($linkedin_text && $linkedin_link): ?>
-                        <li class="socblock-item">
-                            <a href="<?php echo $linkedin_link; ?>" class="socblock-link socblock-link-linkedin" target="_blank" rel="noopener noreferrer">
-                                <svg class="socblock-icon">
-                                    <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprites.svg#icon-linkedin"></use>
-                                </svg>
-                                <span>
-                                    <?php echo $linkedin_text; ?></a>
-                            </span>
                         </li>
-                    <?php endif; ?>
+                    <?php endforeach; ?>
+                        <!-- <?php if ($email_text && $email_link): ?>
+                            <li class="socblock-item">
+                                <a href="mailto:<?php echo $email_link; ?>" class="socblock-link socblock-link-email" target="_blank" rel="noopener noreferrer">
+                                    <svg class="socblock-icon">
+                                        <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprites.svg#icon-email"></use>
+                                    </svg>
+                                    <span>
+                                        <?php echo $email_text; ?></a>
+                                </span>
+                            </li>
+                        <?php endif; ?>
+                        <?php if ($telegram_text && $telegram_link): ?>
+                            <li class="socblock-item">
+                                <a href="<?php echo $telegram_link; ?>" class="socblock-link socblock-link-telegram" target="_blank" rel="noopener noreferrer">
+                                    <svg class="socblock-icon">
+                                        <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprites.svg#icon-telegram"></use>
+                                    </svg>
+                                    <span>
+                                        <?php echo $telegram_text; ?></a>
+                                </span>
+                            </li>
+
+                        <?php endif; ?>
+                        <?php if ($linkedin_text && $linkedin_link): ?>
+                            <li class="socblock-item">
+                                <a href="<?php echo $linkedin_link; ?>" class="socblock-link socblock-link-linkedin" target="_blank" rel="noopener noreferrer">
+                                    <svg class="socblock-icon">
+                                        <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprites.svg#icon-linkedin"></use>
+                                    </svg>
+                                    <span>
+                                        <?php echo $linkedin_text; ?></a>
+                                </span>
+                            </li>
+                        <?php endif; ?> -->
                 </ul>
             </div>
 
@@ -77,7 +110,7 @@ $linkedin_link = esc_html(get_field('linkedin_link', 'options'));
 
         <div class="footer-logo-wrapper">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/star_wish_x-1920.png" alt="" class="footer-logo">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/star-satellite.svg" class="footer-logo-satellite" alt="" >
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/star-satellite.svg" class="footer-logo-satellite" alt="">
         </div>
 
         <div class="footer-copyright">
