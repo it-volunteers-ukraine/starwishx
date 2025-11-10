0b8ccf7d7f70a527d1f6a949e87a55f23a1e2e3a
NilKad <kadulin@gmail.com>
2025-11-10 23:55:12 +0100

    block v1

--- inc/acf/blocks/news/news.php
+++ inc/acf/blocks/news/news.php
@@ -2,12 +2,18 @@
 $default_classes = [
     'section' => 'section',
     'container' => 'container',
+    'title-wrap' => 'title-wrap',
     'title' => 'title',
     'subtitle' => 'subtitle',
     'list' => 'list',
     'item' => 'item',
+    'content' => 'content',
+    'item-title' => 'item-title',
+    'item-label' => 'item-label',
     'block-photo' => 'block-photo',
-    'photo' => 'photo',
+    'item-date' => 'item-date',
+    'photo-wrap' => 'photo-wrap',
+    'photo-img' => 'photo-img',
     'text' => 'text',
     'icon' => 'icon',
     'btn1' => 'btn1',
@@ -18,24 +24,79 @@ $default_classes = [
 $modules_file = get_template_directory() . '/assets/css/blocks/modules.json';
 $classes = $default_classes;
 $title = get_field('title');
-$subtitle = get_field('subtitle');
+$label_text = get_field('label_text');
 $btn_text = get_field('btn_text');
 $btn_url = get_field('btn_page');
 
 $is_mode_click_for_touch = get_field('mode_click_for_touch');
 
-// if ($is_mode_click_for_touch){
-//     print_r("Click mode: true");
+$terms = get_terms([
+    'taxonomy' => 'categories-news',
+    'hide_empty' => false
+]);
 
-// } else{
-//     print_r("Click mode: false");
+
+$results = [];
+
+// Получение с каждой категории по последнему посту
+// foreach ($terms as $term) {
+//     $query = new WP_Query([
+//         'post_type' => 'news',
+//         'posts_per_page' => 1,
+//         'tax_query' => [
+//             [
+//                 'taxonomy' => 'categories-news',
+//                 'field' => 'term_id',
+//                 'terms' => $term->term_id
+//             ]
+//         ],
+//         'orderby' => 'date',
+//         'order' => 'DESC'
+//     ]);
+
+//     if ($query->have_posts()) {
+//         $post_item = $query->posts[0];
+
+//         // добавляем категорию внутрь объекта
+//         $term_post = get_the_terms($post_item->ID, 'categories-news');
+//         $post_item->term_id = $term_post ? $term_post[0]->term_id : null;
+//         $post_item->term_name = $term_post ? $term_post[0]->name : null;
+
+//         // $post_item->category_term = get_the_terms($post_item->ID, 'categories-news')[0]['term_id    '] ?? null;
+
+//         $results[] = $post_item;
+//     }
+
+//     wp_reset_postdata();
 // }
 
-// $photo_url = esc_url(get_field('photo')["sizes"]['large']);
-// echo '<pre>'; // Обертываем в теги для форматирования
-// echo var_dump($items);
-// echo '</pre>';
-// print_r($photo_url);
+$query = new WP_Query([
+    'post_type'      => 'news',
+    'posts_per_page' => 6,   // сколько нужно вывести
+    'orderby'        => 'date',
+    'order'          => 'DESC',
+    'tax_query'      => [
+        [
+            'taxonomy' => 'categories-news',
+            'operator' => 'EXISTS'
+        ]
+    ]
+]);
+
+if ($query->have_posts()) {
+    foreach ($query->posts as $post_item) {
+
+        $term_post = get_the_terms($post_item->ID, 'categories-news');
+        $post_item->term_id = $term_post ? $term_post[0]->term_id : null;
+        $post_item->term_name = $term_post ? $term_post[0]->name : null;
+
+        $results[] = $post_item;
+    }
+}
+
+wp_reset_postdata();
+
+$items = $results;
 
 if (file_exists($modules_file)) {
     $modules = json_decode(file_get_contents($modules_file), true);
@@ -46,31 +107,42 @@ if (file_exists($modules_file)) {
 <section class="section <?php echo esc_attr($classes['section']); ?> ">
     <div class="container ">
         <?php if ($items) : ?>
-            <div class="label-subtitle <?php echo esc_attr($classes['subtitle']); ?>"><?php echo esc_html($subtitle); ?></div>
-            <!-- <div class="<?php echo esc_attr($classes['title-wrap']); ?>"> -->
-            <h2 class="h2-big <?php echo esc_attr($classes['title']); ?>"><?php echo esc_html($main_title); ?></h2>
-            <a href="<?php esc_url($btn_url); ?>" class="btn <?php echo esc_attr($classes['btn1']); ?>"><?php echo esc_html($btn_text); ?></a>
-            <!-- </div> -->
+            <div class="label-subtitle <?php echo esc_attr($classes['subtitle']); ?>"><?php echo esc_html($label_text); ?></div>
+            <div class="<?php echo esc_attr($classes['title-wrap']); ?>">
+                <h2 class="h2-big <?php echo esc_attr($classes['title']); ?>"><?php echo esc_html($title); ?></h2>
+                <a href="<?php esc_url($btn_url); ?>" class="btn <?php echo esc_attr($classes['btn1']); ?>"><?php echo esc_html($btn_text); ?></a>
+            </div>
 
             <div class="<?php echo esc_attr($classes['list']); ?>">
                 <?php foreach ($items as $item) : ?>
                     <?php
-                    $text = $item['text'];
-                    $label = $item['label'];
-                    $photo_url = $item['photo']["sizes"]['large'];
-                    $photo_alt = $item['photo']['alt'] ?: $item['photo']['title'];
+                    $post_id = $item->ID;
+                    $term_id = $item->term_id;
+                    $term_full = get_term($term_id);
+                    $item_taxonomy = $term_full->taxonomy;
+                    $label_color_text = get_field('label_color_text', $item_taxonomy . '_' . $term_id);
+                    $label_color_background = get_field('label_color_background', $item_taxonomy . '_' . $term_id);
+                    $term_name = $item->term_name;
+                    $item_date = date('d.m.Y', strtotime($item->post_date));
+                    $item_title = get_field('title', $post_id);
+
+                    $item_label = esc_html($term_name);
+                    $photo = get_field('photo', $post_id);
+                    $photo_url = $photo["sizes"]['large'];
+                    $photo_alt = $photo['alt'] ?: $photo['title'];
 
                     ?>
                     <div class="<?php echo esc_attr($classes['item']); ?>">
                         <div class="<?php echo esc_attr($classes['content']); ?>">
-                            <!-- <div class="<?php echo esc_attr($classes['block-photo']); ?>"> -->
                             <div class="<?php echo esc_attr($classes['photo-wrap']); ?>">
                                 <img src="<?php echo esc_url($photo_url); ?>" class="<?php echo  esc_attr($classes['photo-img']); ?>" alt="<?php echo $photo_alt; ?>">
+                                <div class="<?php echo esc_attr($classes['item-label']); ?>" style="--label-color: <?php echo $label_color_text; ?>; --label-bg: <?php echo $label_color_background; ?>; "><?php echo $item_label; ?></div>
+
                             </div>
-                            <!-- </div> -->
-                            <?php if ($text) : ?>
-                                <div class="<?php echo esc_attr($classes['text']); ?>">
-                                    <?php echo $text; ?>
+                            <div class="<?php echo esc_attr($classes['item-date']); ?>"><?php echo $item_date; ?></div>
+                            <?php if ($item_title) : ?>
+                                <div class="<?php echo esc_attr($classes['item-title']); ?>">
+                                    <?php echo $item_title; ?>
                                 </div>
                             <?php endif; ?>
                         </div>
@@ -78,7 +150,8 @@ if (file_exists($modules_file)) {
                 <? endforeach; ?>
             </div>
             <a href="<?php esc_url($btn_url); ?>" class="btn <?php echo esc_attr($classes['btn2']); ?>"><?php echo esc_html($btn_text); ?></a>
-            <a href="<?php esc_url($btn_url); ?>" class="btn <?php echo esc_attr($classes['btn']); ?>"><?php echo esc_html($btn_text); ?></a>
         <?php endif; ?>
     </div>
 </section>
+<?php
+?>
