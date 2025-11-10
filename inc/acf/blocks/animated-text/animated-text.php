910c5f4734a60246bc535b546e1ec0893da6f287
teamspace2028 <teamspace2028@gmail.com>
2025-11-10 19:05:47 +0200

    101125

--- inc/acf/blocks/animated-text/animated-text.php
+++ inc/acf/blocks/animated-text/animated-text.php
@@ -1,11 +1,8 @@
 <?php
 $default_classes = [
-  'animated-text-section' => 'animated-text-section',
-  'animated-text-wrapper' => 'animated-text-wrapper',
-  'animated-text-content' => 'animated-text-content',
-  'text-desktop' => 'text-desktop',
-  'text-tablet' => 'text-tablet',
-  'text-mobile' => 'text-mobile',
+  'animated-text-section'  => 'animated-text-section',
+  'animated-text-wrapper'  => 'animated-text-wrapper',
+  'animated-text-content'  => 'animated-text-content',
 ];
 
 $modules_file = get_template_directory() . '/assets/css/blocks/modules.json';
@@ -15,40 +12,18 @@ if (file_exists($modules_file)) {
   $modules = json_decode(file_get_contents($modules_file), true);
   $classes = array_merge($default_classes, $modules['animated-text'] ?? []);
 }
+
+$text = get_field('animated_text');
 ?>
 
-<section class="<?php echo esc_attr( $classes['animated-text-section'] ); ?>">
-  
-    <div class="<?php echo esc_attr( $classes['animated-text-content'] ); ?>">
-
-      
-      <div class="<?php echo esc_attr( $classes['text-desktop'] ); ?>">
-        <span>Зростай разом зі STAR WISH X.</span><br>
-        <span>відкривай нові можливості</span><br>
-        <span> для розвитку та досягай</span><br>
-        <span>своїх цілей з нами.</span>
+<?php if ($text) { ?>
+<section class="<?php echo esc_attr($classes['animated-text-section']); ?>">
+  <div class="container">
+    <div class="<?php echo esc_attr($classes['animated-text-wrapper']); ?>">
+      <div class="<?php echo esc_attr($classes['animated-text-content']); ?>">
+        <p><?php echo get_field('animated_text'); ?></p>
       </div>
-
-      <div class="<?php echo esc_attr( $classes['text-tablet'] ); ?>">
-        <span>Зростай разом зі STAR</span><br>
-        <span>WISH X. відкривай нові</span><br>
-        <span>можливості</span><br>
-        <span>для розвитку та</span><br>
-        <span>досягай</span><br>
-        <span>своїх цілей з нами.</span>
-      </div>
-
-      <div class="<?php echo esc_attr( $classes['text-mobile'] ); ?>">
-        <span>Зростай разом</span><br>
-        <span>зі STAR WISH X.</span><br>
-        <span>відкривай нові</span><br>
-        <span>можливості</span><br>
-        <span>для розвитку та</span><br>
-        <span>досягай</span><br>
-        <span>своїх цілей з</span><br>
-        <span>нами.</span>
-      </div>
-
     </div>
-  
+  </div>
 </section>
+<?php } ?>
