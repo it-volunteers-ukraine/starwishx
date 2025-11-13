5e16a385d89e9bca7c8b73d5c1d29db9f964290d
teamspace2028 <teamspace2028@gmail.com>
2025-11-13 20:12:44 +0200

    131125

--- inc/acf/blocks/animated-text/animated-text.php
+++ inc/acf/blocks/animated-text/animated-text.php
@@ -1,8 +1,12 @@
 <?php
 $default_classes = [
-  'animated-text-section'  => 'animated-text-section',
-  'animated-text-wrapper'  => 'animated-text-wrapper',
-  'animated-text-content'  => 'animated-text-content',
+  'animated-text-section' => 'animated-text-section',
+  'animated-text-wrapper' => 'animated-text-wrapper',
+  'animated-text-content' => 'animated-text-content',
+  'text' => 'text',
+  'text-desktop' => 'text-desktop',
+  'text-tablet' => 'text-tablet',
+  'text-mobile' => 'text-mobile',
 ];
 
 $modules_file = get_template_directory() . '/assets/css/blocks/modules.json';
@@ -12,18 +16,13 @@ if (file_exists($modules_file)) {
   $modules = json_decode(file_get_contents($modules_file), true);
   $classes = array_merge($default_classes, $modules['animated-text'] ?? []);
 }
-
-$text = get_field('animated_text');
 ?>
 
-<?php if ($text) { ?>
-<section class="<?php echo esc_attr($classes['animated-text-section']); ?>">
+<section class="section <?php echo esc_attr($classes['animated-text-section']); ?>">
   <div class="container">
-    <div class="<?php echo esc_attr($classes['animated-text-wrapper']); ?>">
-      <div class="<?php echo esc_attr($classes['animated-text-content']); ?>">
-        <p><?php echo get_field('animated_text'); ?></p>
-      </div>
+    <div class="<?php echo esc_attr($classes['animated-text-content']); ?>">
+      <!-- <p class="text"><?php (get_field('text')); ?></p> -->
+      <?php echo get_field('text'); ?>
     </div>
   </div>
 </section>
-<?php } ?>
