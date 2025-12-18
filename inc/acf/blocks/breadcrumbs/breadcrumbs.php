5db21b55ae0b716fcdbbc674afb5b8c2a87f3722
NilKad <kadulin@gmail.com>
2025-12-19 00:01:09 +0100

    breadcumbs v1

--- inc/acf/blocks/breadcrumbs/breadcrumbs.php
+++ inc/acf/blocks/breadcrumbs/breadcrumbs.php
@@ -5,20 +5,35 @@ $default_classes = [
     'list' => 'list',
     'item' => 'item',
     'selected' => 'selected',
-    'link' => 'link'
+    'link' => 'link',
+    'arrow-icon' => 'arrow-icon',
+    'home-icon' => 'home-icon'
+
 ];
 
-echo 'dasdasda';
-$ur = get_permalink();
-print_r('wp_reques: ', get_permalink());
-
-$modules_file = get_template_directory() . '/assets/css/blocks/modules.json';
 $classes = $default_classes;
-
+$modules_file = get_template_directory() . '/assets/css/blocks/modules.json';
 if (file_exists($modules_file)) {
     $modules = json_decode(file_get_contents($modules_file), true);
     $classes = array_merge($default_classes, $modules['breadcrumbs'] ?? []);
 }
+
+global $post;
+$breadcrumbs = [];
+
+while ($post) {
+    $breadcrumbs[] = [
+        'title' => get_the_title($post),
+        'link'  => get_permalink($post),
+    ];
+
+    if ($post->post_parent) {
+        $post = get_post($post->post_parent);
+    } else {
+        $post = false;
+    }
+}
+$breadcrumbs = array_reverse($breadcrumbs);
 
 // Home page
 $home_id = get_option('page_on_front');
@@ -35,14 +50,21 @@ $active_title = get_the_title();
             <ul class="text-r <?php echo esc_attr($classes["list"]); ?>">
                 <li class="<?php echo esc_attr($classes["item"]); ?>">
                     <a class="link-bc" href="<?php echo esc_url($home_url); ?>">
-                        <?php echo esc_html($home_title); ?>
+                        <svg class="<?php echo esc_attr($classes["home-icon"]); ?>">
+                            <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprites.svg#icon_house"></use>
+                        </svg>
                     </a>
                 </li>
-                <li class="link-bc-selected <?php echo esc_attr($classes["item"]); ?>">
-                    <span>
-                        <?php echo esc_html($active_title); ?>
-                    </span>
-                </li>
+                <?php foreach ($breadcrumbs as $crumb): ?>
+                    <li class="<?php echo esc_attr($classes["item"]); ?>">
+                        <svg class="<?php echo esc_attr($classes["arrow-icon"]); ?>">
+                            <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprites.svg#icon-arrow"></use>
+                        </svg>
+                        <a class="link-bc" href="<?php echo esc_url($crumb['link']); ?>">
+                            <?php echo esc_html($crumb['title']); ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
             </ul>
         </nav>
     </div>
