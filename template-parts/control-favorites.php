697936a7e42083ee91f17fcf936699b3b91f6d16
shim128 <shimkonn@gmail.com>
2026-01-30 08:41:57 +0100

    feat(ui): update favorites control UI with heart-label

--- template-parts/control-favorites.php
+++ template-parts/control-favorites.php
@@ -2,10 +2,12 @@
 
 /**
  * Favorites Control (Frontend Version)
- * Namespace: starwishx/opportunities
+ * 
+ * File: template-parts/control-favorites.php
  */
 
 declare(strict_types=1);
+
 $post_id = $args['post_id'] ?? get_the_ID();
 ?>
 
@@ -13,29 +15,6 @@ $post_id = $args['post_id'] ?? get_the_ID();
     class="control-favorites-wrapper"
     data-wp-interactive="starwishx/opportunities"
     data-wp-context='{ "id": <?= (int)$post_id ?> }'>
-
-    <!-- <input
-        type="checkbox"
-        class="btn-favorite__checkbox"
-        id="favorite-<?= esc_attr($post_id) ?>"
-        data-id="<?= esc_attr($post_id) ?>"
-        data-wp-bind--checked="state.isFavorite"
-        data-wp-on--change="actions.toggle"
-        aria-label="<?php esc_attr_e('Toggle Favorite', 'starwishx'); ?>">
-
-    <label
-        for="favorite-<?= esc_attr($post_id) ?>"
-        class="btn-favorite"
-        data-wp-class--is-active="state.isFavorite">
-        <span class="btn-favorite__text">
-            <span class="btn-favorite__text--inactive"><?php esc_html_e('Add to favorites', 'starwishx'); ?></span>
-            <span class="btn-favorite__text--active"><?php esc_html_e('In favorites', 'starwishx'); ?></span>
-        </span>
-        <span class="btn-favorite__icon" aria-hidden="true">
-            <span class="btn-favorite__icon--inactive">🤍</span>
-            <span class="btn-favorite__icon--active">💛</span>
-        </span>
-    </label> -->
     <label
         for="favorite-<?= esc_attr($post_id) ?>"
         class="heart-label"
